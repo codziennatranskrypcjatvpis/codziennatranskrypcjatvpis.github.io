24a4b08fec93cd6aab5135ca0d6e18e78be5df0a
codziennatranskrypcjatvpis <85027700+codziennatranskrypcjatvpis@users.noreply.github.com>
2021-05-29 17:00:35 +0200

    Initial commit

---
+++ FAQ.md
@@ -0,0 +1,7 @@
+## FAQ
+# Jak to działa? 
+Od strony sprzętowej: Raspberry Pi 3B i tuner DVB-T. Od strony oprogramowania: TVheadend, CCExtractor wyciagający napisy z teletekstu (dokładniej strony 777) i autorski [skrypt](https://github.com/codziennatranskrypcjatvpis/codziennatranskrypcjatvpis.github.io/blob/main/napisy.sh) do oczyszczenia wygenerowanych napisów, a później do wrzucenia ich na GitHuba. Z przyczyn technicznych skrypt ma tutaj nieprawidłowo zapisane polskie znaki, należy to poprawić przed użyciem go u siebie.
+# Co jeśli chcę użyć transkrypcji z tej strony do własnego projektu?
+Tak, daj tylko link do strony i miło będzie, jeśli o tym poinformujesz.
+# Jak się narodził ten projekt?
+Natrafiłem na stary post @FlasH z zapytaniem, czy ktoś umie wyciągać napisy emitowane w teletekście. Zacząłem szukać takiej możliwości i rozmawiać z nim, 2 dni później codzienna transkrypcja ruszyła.
---
+++ napisy.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+rm /media/Recordings/Wiadomosci_archiwum/*.ts
+/usr/local/bin/ccextractor *.ts -nofc -nots -tpage 778 -o Wiadomosci.srt
+grep -v 'STRONA 777' Wiadomosci.srt | grep -v 'NAPISY DLA NIESŁYSZĄCYCH' | grep -v 'WYKAZ PROGRAM' | grep -v ",,," | grep -v 'KOLEJNA EMISJA' | grep -v " / " | sponge  Wiadomosci.srt
+grep -E "[A-Za-z]" Wiadomosci.srt | tr '\r\n' ' ' | tr -s \ > transcriptions_txt/Wiadomosci_$(date +%d.%m.%Y).txt
+mv Wiadomosci.srt transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt
+rm README.md
+touch README.md
+echo "* [Wiadomosci_$(date +%d.%m.%Y).srt](https://raw.githubusercontent.com/codziennatranskrypcjatvpis/codziennatranskrypcjatvpis.github.io/main/transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt)" | cat - lista.md | sponge lista.md
+echo "* [Wiadomosci_$(date +%d.%m.%Y).txt](https://raw.githubusercontent.com/codziennatranskrypcjatvpis/codziennatranskrypcjatvpis.github.io/main/transcriptions_txt/Wiadomosci_$(date +%d.%m.%Y).txt)" | cat - lista.md | sponge lista.md
+echo "[FAQ](https://github.com/codziennatranskrypcjatvpis/codziennatranskrypcjatvpis.github.io/blob/main/FAQ.md)" | cat - lista.md | sponge README.md
+git add transcriptions_txt/Wiadomosci_$(date +%d.%m.%Y).txt
+git add transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt
+git add README.md
+git commit -m "Nowe wydanie"
+git push origin
+mv *.ts /media/Recordings/Wiadomosci_archiwum/
