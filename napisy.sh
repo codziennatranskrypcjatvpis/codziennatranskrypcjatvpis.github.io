71b451123c9e84567d3778c7d6d965f2eb443cfe
codziennatranskrypcjatvpis <codziennatranskrypcja@wp.pl>
2025-03-23 19:59:59 +0100

    poprawa skryptu

--- napisy.sh
+++ napisy.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-rm /media/Recordings/Wiadomosci_archiwum/*.ts
-/usr/local/bin/ccextractor $(ls -Art | grep \.ts | tail -n 1) -nofc -nots -tpage 778 -o Wiadomosci.srt
+/usr/local/bin/ccextractor "$1" -nofc -nots -tpage 778 -o Wiadomosci.srt
 grep -v 'STRONA 777' Wiadomosci.srt | grep -v 'NAPISY DLA NIESŁYSZĄCYCH' | grep -v 'WYKAZ PROGRAM' | grep -v ",,," | grep -v 'KOLEJNA EMISJA' | grep -v " / " | sponge Wiadomosci.srt
 grep -E "[A-Za-z]" Wiadomosci.srt | tr '\r\n' ' ' | tr -s \ > transcriptions_txt/Wiadomosci_$(date +%d.%m.%Y).txt
 mv Wiadomosci.srt transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt
