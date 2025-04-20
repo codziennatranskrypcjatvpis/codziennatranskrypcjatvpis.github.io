cdb13212e38ef5051042350c060f0ec49447ef2e
codziennatranskrypcjatvpis <codziennatranskrypcja@wp.pl>
2025-04-20 20:15:15 +0200

    fix

--- napisy.sh
+++ napisy.sh
@@ -1,15 +1,6 @@
 #!/bin/bash
 /usr/local/bin/ccextractor "$1" -nofc -nots -tpage 778 -o Wiadomosci.srt
-grep -v 'STRONA 777' Wiadomosci.srt | grep -v 'NAPISY DLA NIESŁYSZĄCYCH' | grep -v 'WYKAZ PROGRAM' | grep -v ",,," | grep -v 'KOLEJNA EMISJA' | grep -v " / " | sponge Wiadomosci.srt
-grep -E "[A-Za-z]" Wiadomosci.srt | tr '\r\n' ' ' | tr -s \ > transcriptions_txt/Wiadomosci_$(date +%d.%m.%Y).txt
-mv Wiadomosci.srt transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt
-rm README.md
-touch README.md
-echo "* [Wiadomosci_$(date +%d.%m.%Y).srt](https://raw.githubusercontent.com/codziennatranskrypcjatvpis/codziennatranskrypcjatvpis.github.io/main/transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt)" | cat - lista.md | sponge lista.md
-echo "* [Wiadomosci_$(date +%d.%m.%Y).txt](https://raw.githubusercontent.com/codziennatranskrypcjatvpis/codziennatranskrypcjatvpis.github.io/main/transcriptions_txt/Wiadomosci_$(date +%d.%m.%Y).txt)" | cat - lista.md | sponge lista.md
-echo "[FAQ](https://github.com/codziennatranskrypcjatvpis/codziennatranskrypcjatvpis.github.io/blob/main/FAQ.md)" | cat - lista.md | sponge README.md
-git add transcriptions_txt/Wiadomosci_$(date +%d.%m.%Y).txt
-git add transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt
-git add README.md
-git commit -m "Nowe wydanie"
-git push origin
+grep -v 'STRONA 777' Wiadomosci.srt | grep -v 'NAPISY DLA NIESŁYSZĄCYCH' | grep -v 'WYKAZ PROGRAM' | grep -v ",,," | grep -v 'KOLEJNA EMISJA' | grep -v " / " > transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt
+grep -E "[A-Za-z]" transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt | tr '\r\n' ' ' | tr -s \ > transcriptions_txt/Wiadomosci_$(date +%d.%m.%Y).txt
+echo "* [Wiadomosci_$(date +%d.%m.%Y).srt](https://raw.githubusercontent.com/codziennatranskrypcjatvpis/codziennatranskrypcjatvpis.github.io/main/transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt)" | cat - README.md | sponge README.md
+echo "* [Wiadomosci_$(date +%d.%m.%Y).txt](https://raw.githubusercontent.com/codziennatranskrypcjatvpis/codziennatranskrypcjatvpis.github.io/main/transcriptions_txt/Wiadomosci_$(date +%d.%m.%Y).txt)" | cat - README.md | sponge README.md
