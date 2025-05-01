2074db7d9f9a0d49eedc6712a1f7d1a2b42258b8
codziennatranskrypcjatvpis <codziennatranskrypcja@wp.pl>
2025-05-01 15:44:15 +0200

    update script

--- napisy.sh
+++ napisy.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-/usr/local/bin/ccextractor "$1" -nofc -nots -tpage 778 -o Wiadomosci.srt
-grep -v 'STRONA 777' Wiadomosci.srt | grep -v 'NAPISY DLA NIESŁYSZĄCYCH' | grep -v 'WYKAZ PROGRAM' | grep -v ",,," | grep -v 'KOLEJNA EMISJA' | grep -v " / " > transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt
+/usr/local/bin/ccextractor "$1" -nofc -nots -tpage 778 -o /tmp/Wiadomosci.srt
+cat /tmp/Wiadomosci.srt | grep -v 'STRONA 777' | grep -v 'NAPISY DLA NIESŁYSZĄCYCH' | grep -v 'WYKAZ PROGRAM' | grep -v ",,," | grep -v 'KOLEJNA EMISJA' | grep -v " / " > transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt
 grep -E "[A-Za-z]" transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt | tr '\r\n' ' ' | tr -s \ > transcriptions_txt/Wiadomosci_$(date +%d.%m.%Y).txt
 echo "* [Wiadomosci_$(date +%d.%m.%Y).srt](https://raw.githubusercontent.com/codziennatranskrypcjatvpis/codziennatranskrypcjatvpis.github.io/main/transcriptions_srt/Wiadomosci_$(date +%d.%m.%Y).srt)" | cat - README.md | sponge README.md
 echo "* [Wiadomosci_$(date +%d.%m.%Y).txt](https://raw.githubusercontent.com/codziennatranskrypcjatvpis/codziennatranskrypcjatvpis.github.io/main/transcriptions_txt/Wiadomosci_$(date +%d.%m.%Y).txt)" | cat - README.md | sponge README.md
+./push_to_git.sh
