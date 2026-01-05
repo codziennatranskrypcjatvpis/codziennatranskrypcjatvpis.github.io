bdeb15a339ea1768bb5f125d6e40f4dd0030c403
codziennatranskrypcjatvpis <codziennatranskrypcja@wp.pl>
2026-01-05 17:22:18 +0100

    Wyszukiwarka

--- Wyszukiwarka/load.js
+++ Wyszukiwarka/load.js
@@ -3,7 +3,8 @@ const fs = require('fs');
 const dir = fs.opendirSync('../transcriptions_txt')
 let dirent
 while ((dirent = dir.readSync()) !== null) {
-  const data = fs.readFileSync(`../transcriptions_txt/${dirent.name}`, { encoding: 'utf8', flag: 'r' }).replace("'", "''");;
-  console.log(`${dirent.name};${data}`);
+  const text = fs.readFileSync(`../transcriptions_txt/${dirent.name}`, { encoding: 'utf8', flag: 'r' }).replace("'", "''");
+  const date = dirent.name.substring(17, 21) + "-" + dirent.name.substring(14, 16) + "-" + dirent.name.substring(11, 13);
+  console.log(`${date};;;;;${dirent.name};;;;;${text}`);
 }
 dir.closeSync()
