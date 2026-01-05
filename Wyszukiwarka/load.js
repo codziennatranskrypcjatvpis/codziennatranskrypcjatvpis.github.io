d8a74d345c55b66924195911ace7a09c27eb67c4
codziennatranskrypcjatvpis <codziennatranskrypcja@wp.pl>
2026-01-05 17:31:31 +0100

    Wyszukiwarka

--- Wyszukiwarka/load.js
+++ Wyszukiwarka/load.js
@@ -5,6 +5,6 @@ let dirent
 while ((dirent = dir.readSync()) !== null) {
   const text = fs.readFileSync(`../transcriptions_txt/${dirent.name}`, { encoding: 'utf8', flag: 'r' }).replace("'", "''");
   const date = dirent.name.substring(17, 21) + "-" + dirent.name.substring(14, 16) + "-" + dirent.name.substring(11, 13);
-  console.log(`${date};;;;;${dirent.name};;;;;${text}`);
+  console.log(`${date}~${dirent.name}~${text}`);
 }
 dir.closeSync()
